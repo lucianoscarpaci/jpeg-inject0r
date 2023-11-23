817a2ba8b36fe09405d0e25427ed826e9f6ff5c7
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2023-11-23 16:12:42 -0500

    Add the key to the program

--- aes.rb
+++ aes.rb
@@ -1,5 +1,6 @@
 require 'openssl'
 require 'base64'
+require 'dotenv/load'
 
 def encrypt(plaintext, key, iv)
   cipher = OpenSSL::Cipher.new('AES-256-CBC')
@@ -11,9 +12,22 @@ def encrypt(plaintext, key, iv)
   encrypted.unpack1('H*')
 end
 
+
+def duplicate_and_cut(original_key, desired_length)
+  duplicated_key = original_key.chars.cycle.take(desired_length).join
+  duplicated_key.slice(0, desired_length)
+end
+
+original_key = ENV['KEY']
+desired_length = 32 # Length of the target key
+
+target_key = duplicate_and_cut(original_key, desired_length)
+puts target_key
+
 plaintext = 'spend outside coin quarter drink provide water stove spare device novel sweet'
-key = OpenSSL::Random.random_bytes(32)
-puts "Key: #{key.unpack1('H*')}"
+
+key = target_key
+#puts "Key: #{key.unpack1('H*')}"
 iv = OpenSSL::Cipher.new('AES-256-CBC').random_iv
 
 encrypted = encrypt(plaintext, key, iv)
