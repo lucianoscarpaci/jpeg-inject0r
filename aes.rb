54211f7b3389238614987ec970077bcf66d49f55
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2023-11-24 00:20:58 -0500

    Add padding, encrypt and decrypt functions
    Add pad encode, and pad decode and the encrypt and decrypt functions

--- aes.rb
+++ aes.rb
@@ -2,34 +2,59 @@ require 'openssl'
 require 'base64'
 require 'dotenv/load'
 
-def encrypt(plaintext, key, iv)
-  cipher = OpenSSL::Cipher.new('AES-256-CBC')
-  cipher.encrypt
-  cipher.key = key
-  cipher.iv = iv
-
-  encrypted = cipher.update(plaintext) + cipher.final
-  encrypted.unpack1('H*')
-end
-
+original_key = ENV['KEY']
+desired_length = 32
 
 def duplicate_and_cut(original_key, desired_length)
   duplicated_key = original_key.chars.cycle.take(desired_length).join
   duplicated_key.slice(0, desired_length)
 end
 
-original_key = ENV['KEY']
-desired_length = 32 # Length of the target key
-
-target_key = duplicate_and_cut(original_key, desired_length)
-puts target_key
-
-plaintext = 'spend outside coin quarter drink provide water stove spare device novel sweet'
-
-key = target_key
-#puts "Key: #{key.unpack1('H*')}"
+key = duplicate_and_cut(original_key, desired_length)
 iv = OpenSSL::Cipher.new('AES-256-CBC').random_iv
 
-encrypted = encrypt(plaintext, key, iv)
-encoded = Base64.strict_encode64(encrypted)
-puts "Encrypted: #{encoded}"
+def pad_encoded(encoded)
+  if encoded[-1] == '='
+    encoded += '=' unless encoded[-2] == '='
+  else
+    encoded += '=='
+  end
+  encoded
+end
+
+def unpad_decoded(decoded)
+  decoded = decoded.chomp('=')
+  decoded = decoded.chomp('=') unless decoded[-1] == '='
+  decoded
+end
+
+def encrypt(plaintext, key, iv)
+  cipher = OpenSSL::Cipher.new('AES-256-CBC')
+  cipher.encrypt
+  cipher.key = key
+  cipher.iv = iv
+  encrypted = cipher.update(plaintext) + cipher.final
+  encrypted.unpack1('H*')
+  encoded = Base64.strict_encode64(encrypted)
+  encoded = pad_encoded(encoded)
+end
+
+def decrypt(ciphertext, key, iv)
+ cipher = OpenSSL::Cipher.new('AES-256-CBC')
+ cipher.decrypt
+ cipher.key = key
+ cipher.iv = iv
+ decoded = Base64.strict_decode64(unpad_decoded(ciphertext))
+ decrypted = cipher.update(decoded) + cipher.final
+end
+
+print 'New message: '
+plaintext = gets.chomp
+
+# Encryption
+ciphertext = encrypt(plaintext, key, iv)
+puts "Ciphertext: #{ciphertext}"
+
+# Decryption
+decrypted_text = decrypt(ciphertext, key, iv)
+puts "Decrypted Text: #{decrypted_text}"
